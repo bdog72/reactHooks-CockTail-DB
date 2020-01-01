1454ce85078ce765cc7f70bb91143f61b073eb32
Brian Bycynski <high5bri@hotmail.com>
2020-01-01 06:52:22 -0500

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+//
+//
+import React from 'react';
+
+// import pages
+import Home from './pages/Home';
+import About from './pages/About';
+import SingleCocktail from './pages/SingleCocktail';
+import Error from './pages/Error';
+
+export default function App() {
+  return (
+    <>
+      <Home />
+      <About />
+      <SingleCocktail />
+      <Error />
+    </>
+  );
+}
